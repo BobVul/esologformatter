65e2af0fcd05202531a66f2a520f11192a7c033c
Bob <bob+git@vulpin.com>
2019-03-03 03:50:42 +1100

    Add import command

---
+++ EsoLogFormatter/Models/Log.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EsoLogFormatter.Models
+{
+    class Log
+    {
+
+
+        public Log() { }
+
+        public Log(string filePath)
+        {
+            string[] lines = File.ReadAllLines(filePath);
+        }
+    }
+}
--- EsoLogFormatter/ViewModels/HomeViewModel.cs
+++ EsoLogFormatter/ViewModels/HomeViewModel.cs
@@ -1,16 +1,35 @@
-﻿using System;
+﻿using EsoLogFormatter.Helpers;
+using EsoLogFormatter.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
 
 namespace EsoLogFormatter.ViewModels
 {
     class HomeViewModel
     {
         public FileSelectorViewModel FileSelector { get; set; }
+        public Log CurrentLog { get; set; }
+
+        public ICommand ImportCommand { get; set; }
 
         public HomeViewModel()
         {
             FileSelector = new FileSelectorViewModel();
+
+            ImportCommand = new RelayCommand(() =>
+            {
+                try
+                {
+                    CurrentLog = new Log(FileSelector.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Log parse error");
+                }
+            });
         }
     }
 }
