62a757e1cabfa0c969d8be7afbb1a93394666e99
Bob <bob+git@vulpin.com>
2019-03-03 01:58:20 +1100

    Add WPF views and file selection demo

---
+++ EsoLogFormatter/ViewModels/FileSelectorViewModel.cs
@@ -0,0 +1,24 @@
+﻿using EsoLogFormatter.Helpers;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace EsoLogFormatter.ViewModels
+{
+    class FileSelectorViewModel
+    {
+        public string FileName { get; set; }
+        public ICommand BrowseCommand { get; set; }
+
+        public FileSelectorViewModel()
+        {
+            BrowseCommand = new RelayCommand(() =>
+            {
+                var dialog = new OpenFileDialog();
+                dialog.ShowDialog();
+            });
+        }
+    }
+}
---
+++ EsoLogFormatter/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EsoLogFormatter.ViewModels
+{
+    class HomeViewModel
+    {
+        public FileSelectorViewModel FileSelector { get; set; }
+
+        public HomeViewModel()
+        {
+            FileSelector = new FileSelectorViewModel();
+        }
+    }
+}
